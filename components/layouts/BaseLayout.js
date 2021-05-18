6e9fec524aed650dfa14363641123d37484585cb
Bui Duong Kha Minh <minhbdk@payme.vn>
2021-05-18 17:48:18 +0700

    base layout

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -3,10 +3,14 @@ import Header from "../shared/Header";
 
 function BaseLayout(props) {
   return (
-    <>
+    <div className="layout-container">
       <Header />
-      {props.children}
-    </>
+      <main className="cover">
+        <div className="wrapper">
+          {props.children}
+        </div>
+      </main>
+    </div>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,45 @@
-import { Alert } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello Home</h1>
-      <div>
-        <Alert color="success">
-          <h4 className="alert-heading">Well done!</h4>
-          <p>
-            Aww yeah, you successfully read this important alert message. This
-            example text is going to run a bit longer so that you can see how
-            spacing within an alert works with this kind of content.
-          </p>
-          <hr />
-          <p className="mb-0">
-            Whenever you need to, be sure to use margin utilities to keep things
-            nice and tidy.
-          </p>
-        </Alert>
+    <div className="main-section">
+      <div className="background-image">
+        <img src="images/background-index.png" />
       </div>
-    </>
+      <Container>
+        <Row>
+          <Col md="6">
+            <div className="hero-section">
+              <div className={`flipper`}>
+                <div className="back">
+                  <div className="hero-section-content">
+                    <h2> Full Stack Web Developer </h2>
+                    <div className="hero-section-content-intro">
+                      Have a look at my portfolio and job history.
+                    </div>
+                  </div>
+                  <img className="image" src="images/section-1.png" />
+                  <div className="shadow-custom">
+                    <div className="shadow-inner"> </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md="6" className="hero-welcome-wrapper">
+            <div className="hero-welcome-text">
+              <h1>
+                Welcome to the portfolio website of Filip Jerga. Get informed,
+                collaborate and discover projects I was working on through the
+                years!
+              </h1>
+            </div>
+            <div className="hero-welcome-bio">
+              <h1>Let's take a look on my work.</h1>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
